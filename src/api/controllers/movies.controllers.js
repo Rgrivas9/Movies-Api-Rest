951288f547bf3bede7391e88e034a77473caba73
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-03-05 11:41:38 +0100

    paginación movies

--- src/api/controllers/movies.controllers.js
+++ src/api/controllers/movies.controllers.js
@@ -2,18 +2,75 @@ const Movie = require("../models/movie.model");
 const { deleteImgCloudinary } = require("../../middlewares/files.middleware");
 const getAllMovies = async (req, res, next) => {
   try {
-    const movies = await Movie.find()
-      .populate("directed_by")
-      .populate("written_by")
-      .populate("produced_by")
-      .populate("music_by")
-      .populate("cast")
-      .populate("awards");
-    return res.status(200).json(movies);
+    if (req.query.page && !isNaN(parseInt(req.query.page))) {
+      const numMovies = await Movie.countDocuments();
+      let page = parseInt(req.query.page);
+      let limit = req.query.limit ? parseInt(req.query.limit) : 1;
+      let numPages =
+        numMovies % limit > 0 ? numMovies / limit + 1 : numMovies / limit;
+      if (page > numPages || page < 1) {
+        page = 1;
+      }
+      const skip = (page - 1) * limit;
+      const movies = await Movie.find()
+        .skip(skip)
+        .limit(limit)
+        .populate("directed_by")
+        .populate("written_by")
+        .populate("produced_by")
+        .populate("music_by")
+        .populate("cast")
+        .populate("awards");
+      return res.status(200).json({
+        info: {
+          total: numMovies,
+          page: page,
+          limit: limit,
+          next:
+            numPages >= page + 1
+              ? `http://localhost:8080/api/v1/movies?page=${
+                  page + 1
+                }&limit=${limit}`
+              : null,
+          prev:
+            page != 1
+              ? `http://localhost:8080/api/v1/movies?page=${
+                  page - 1
+                }&limit=${limit}`
+              : null,
+        },
+        results: movies,
+      });
+    } else {
+      const movies = await Movie.find()
+        .limit(1)
+        .populate("directed_by")
+        .populate("written_by")
+        .populate("produced_by")
+        .populate("music_by")
+        .populate("cast")
+        .populate("awards");
+      const numMovies = await Movie.countDocuments();
+
+      return res.status(200).json({
+        info: {
+          total: numMovies,
+          page: 1,
+          limit: 1,
+          next:
+            numMovies > 1
+              ? `http://localhost:8080/api/v1/movies?page=2&limit=1`
+              : null,
+          prev: null,
+        },
+        results: movies,
+      });
+    }
   } catch (error) {
-    return next(error);
+    return next("Cannot find movies", error);
   }
 };
+
 const getMovieById = async (req, res, next) => {
   try {
     const { id } = req.params;
